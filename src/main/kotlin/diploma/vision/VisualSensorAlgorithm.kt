62cd536d398e4bfeb1eca36889f5db6489642057
Komdosh <Komdosh@yandex.ru>
2018-04-09 07:24:06 +0300

    add visual sensor algorithm class

---
+++ src/main/kotlin/diploma/vision/VisualSensorAlgorithm.kt
@@ -0,0 +1,41 @@
+package diploma.vision
+
+class VisualSensorAlgorithm {
+
+
+  fun countTickUntilAction(): Int {
+    return 0
+  }
+
+  fun countSensorTickUntilAction(): Int {
+    return 0
+  }
+
+  fun calculateMaxVisibleAngle(): Int {
+    return 0
+  }
+
+  fun countSensorTickWithoutMainObject(): Int {
+    return 0
+  }
+
+  fun estimateSensorTickWithoutMainObject(): Int {
+    return 0
+  }
+
+  fun getLowQualityVisualInfo() {
+
+  }
+
+  fun countVisualTickForEstimate(): Int {
+    return 0
+  }
+
+  fun estimateUsefulnessAngles(): Int {
+    return 0
+  }
+
+  fun getHighQualityVisualInfo() {
+
+  }
+}
